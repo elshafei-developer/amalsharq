98fc395ba77589a32aae1a982c8fe21cf3d4b09c
elshafei-developer <el.shafei.developer@gmail.com>
2024-07-07 12:20:30 +0300

    use getCenter() to get Center Point

--- amalsharq/overrides/location.py
+++ amalsharq/overrides/location.py
@@ -1,9 +1,10 @@
 from erpnext.assets.doctype.location.location import Location
 import json
 import frappe
+from frappe import _
 class CustomLocation(Location):
     def before_insert(self):
-        location_dic = json.loads(self.location)
-        type_point = location_dic['features'][-1]['geometry']['type']
+        location_dict = json.loads(self.location)
+        type_point = location_dict['features'][-1]['geometry']['type']
         if type_point != "Point":
             frappe.throw(_("ERROR Only points of 'point' type are accepted"))
--- amalsharq/public/js/location.js
+++ amalsharq/public/js/location.js
@@ -15,22 +15,20 @@ frappe.ui.form.on("Location", {
       }
     }
     if (!frm.is_new()) {
-      let latitude = +frm.doc.latitude;
-      let longitude = +frm.doc.longitude;
-
-      if (latitude && longitude) {
-        let map = frm.fields_dict.location.map.setView(
-          [latitude, longitude],
-          13
-        );
-        L.marker([latitude, longitude])
-          .addTo(map)
-          .bindPopup(frm.doc.name)
-          .openPopup();
-      }
     }
   },
   before_save(frm) {
+    let location = JSON.parse(frm.doc.location);
+    let last_point = location.features.slice(-1).pop();
+    let type_point = last_point.geometry.type;
+    if (type_point === "Point") {
+      let latlng = last_point.geometry.coordinates;
+      latitude = +latlng[1];
+      longitude = +latlng[0];
+    } else {
+      latitude = +frm.doc.latitude;
+      longitude = +frm.doc.longitude;
+    }
     frm.doc.location = "";
     let geojsonFeature = {
       type: "FeatureCollection",
@@ -38,11 +36,12 @@ frappe.ui.form.on("Location", {
         {
           type: "Feature",
           properties: {},
-          geometry: { type: "Point", coordinates: [+longitude, +latitude] },
+          geometry: { type: "Point", coordinates: [longitude, latitude] },
         },
       ],
     };
     frm.doc.location = JSON.stringify(geojsonFeature);
+    frm.refresh();
   },
   latitude(frm) {
     refresh_location(frm);
@@ -53,32 +52,13 @@ frappe.ui.form.on("Location", {
   },
 
   location(frm) {
-    let location = JSON.parse(frm.doc.location);
-    let last_point = location.features.slice(-1).pop();
-    let type_point = last_point.geometry.type;
-    if (type_point === "Point") {
-      let latlng = last_point.geometry.coordinates;
-      latitude = +latlng[1];
-      longitude = +latlng[0];
-      frm.doc.location = "";
-      frm.doc.latitude = latitude;
-      frm.doc.longitude = longitude;
-
-      frm.refresh_field("location");
-      frm.refresh_field("latitude");
-      frm.refresh_field("longitude");
-
-      let map = frm.fields_dict.location.map.setView([latitude, longitude], 13);
-      L.marker([latitude, longitude]).addTo(map);
-    } else {
-      frm.doc.location = "";
-      frm.refresh_field("location");
-    }
+    let center = frm.fields_dict.location.map.getCenter();
+    frm.set_value("latitude", center.lat);
+    frm.set_value("longitude", center.lng);
+    frm.refresh();
   },
 });
 function refresh_location(frm) {
-  frm.doc.location = "";
-  frm.refresh_field("location");
   latitude = +frm.doc.latitude;
   longitude = +frm.doc.longitude;
   let map = frm.fields_dict.location.map.setView([latitude, longitude], 13);
