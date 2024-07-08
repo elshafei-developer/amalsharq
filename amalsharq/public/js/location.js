fae9ac708dcc6ee3936564786ab7b1f8842ef350
elshafei-developer <el.shafei.developer@gmail.com>
2024-07-08 13:09:20 +0000

    remove all control drow only point by document javaScrip

--- amalsharq/public/js/location.js
+++ amalsharq/public/js/location.js
@@ -1,18 +1,18 @@
 frappe.ui.form.on("Location", {
   refresh: function (frm) {
-    // const classesToRemove = [
-    //   "leaflet-draw-draw-polyline",
-    //   "leaflet-draw-draw-polygon",
-    //   "leaflet-draw-draw-rectangle",
-    //   "leaflet-draw-draw-circle",
-    //   "leaflet-draw-draw-circlemarker",
-    // ];
-    // for (i in classesToRemove) {
-    //   const elementToRemove = document.querySelector(`a.${classesToRemove[i]}`);
-    //   if (elementToRemove) {
-    //     elementToRemove.remove();
-    //   }
-    // } 
+    const classesToRemove = [
+      "leaflet-draw-draw-polyline",
+      "leaflet-draw-draw-polygon",
+      "leaflet-draw-draw-rectangle",
+      "leaflet-draw-draw-circle",
+      "leaflet-draw-draw-circlemarker",
+    ];
+    for (i in classesToRemove) {
+      const elementToRemove = document.querySelector(`a.${classesToRemove[i]}`);
+      if (elementToRemove) {
+        elementToRemove.remove();
+      }
+    }
   },
   before_save(frm) {
     let location = JSON.parse(frm.doc.location);
@@ -51,24 +51,23 @@ frappe.ui.form.on("Location", {
   location(frm) {
     let location = frm.fields_dict.location;
     let center = location.map.getCenter();
-    location.clear_editable_layers()
+    location.clear_editable_layers();
     frm.set_value("latitude", center.lat);
     frm.set_value("longitude", center.lng);
-    // const classesToRemove = [
-    //     "leaflet-draw-draw-polyline",
-    //     "leaflet-draw-draw-polygon",
-    //     "leaflet-draw-draw-rectangle",
-    //     "leaflet-draw-draw-circle",
-    //     "leaflet-draw-draw-circlemarker",
-    //   ];
-    //   for (i in classesToRemove) {
-    //     const elementToRemove = document.querySelector(`a.${classesToRemove[i]}`);
-    //     if (elementToRemove) {
-    //       elementToRemove.remove();
-    //     }
-    //   } 
-
-},
+    const classesToRemove = [
+      "leaflet-draw-draw-polyline",
+      "leaflet-draw-draw-polygon",
+      "leaflet-draw-draw-rectangle",
+      "leaflet-draw-draw-circle",
+      "leaflet-draw-draw-circlemarker",
+    ];
+    for (i in classesToRemove) {
+      const elementToRemove = document.querySelector(`a.${classesToRemove[i]}`);
+      if (elementToRemove) {
+        elementToRemove.remove();
+      }
+    }
+  },
 });
 function refresh_location(frm) {
   latitude = +frm.doc.latitude;
