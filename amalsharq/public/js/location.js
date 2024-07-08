c7379eaa1077e3a25e62c3ff8524b80cdff27f42
elshafei-developer <el.shafei.developer@gmail.com>
2024-07-08 10:44:35 +0000

    use clear_editable_layers function from geolocation.js

--- amalsharq/public/js/location.js
+++ amalsharq/public/js/location.js
@@ -1,21 +1,18 @@
 frappe.ui.form.on("Location", {
   refresh: function (frm) {
-    const classesToRemove = [
-      "leaflet-draw-draw-polyline",
-      "leaflet-draw-draw-polygon",
-      "leaflet-draw-draw-rectangle",
-      "leaflet-draw-draw-circle",
-      "leaflet-draw-draw-circlemarker",
-    ];
-
-    for (let i = 0; i < classesToRemove.length; i++) {
-      const elementToRemove = document.querySelector(`a.${classesToRemove[i]}`);
-      if (elementToRemove) {
-        elementToRemove.remove();
-      }
-    }
-    if (!frm.is_new()) {
-    }
+    // const classesToRemove = [
+    //   "leaflet-draw-draw-polyline",
+    //   "leaflet-draw-draw-polygon",
+    //   "leaflet-draw-draw-rectangle",
+    //   "leaflet-draw-draw-circle",
+    //   "leaflet-draw-draw-circlemarker",
+    // ];
+    // for (i in classesToRemove) {
+    //   const elementToRemove = document.querySelector(`a.${classesToRemove[i]}`);
+    //   if (elementToRemove) {
+    //     elementToRemove.remove();
+    //   }
+    // } 
   },
   before_save(frm) {
     let location = JSON.parse(frm.doc.location);
@@ -52,15 +49,41 @@ frappe.ui.form.on("Location", {
   },
 
   location(frm) {
-    let center = frm.fields_dict.location.map.getCenter();
+    let location = frm.fields_dict.location;
+    let center = location.map.getCenter();
+    location.clear_editable_layers()
     frm.set_value("latitude", center.lat);
     frm.set_value("longitude", center.lng);
-    frm.refresh();
-  },
+    // const classesToRemove = [
+    //     "leaflet-draw-draw-polyline",
+    //     "leaflet-draw-draw-polygon",
+    //     "leaflet-draw-draw-rectangle",
+    //     "leaflet-draw-draw-circle",
+    //     "leaflet-draw-draw-circlemarker",
+    //   ];
+    //   for (i in classesToRemove) {
+    //     const elementToRemove = document.querySelector(`a.${classesToRemove[i]}`);
+    //     if (elementToRemove) {
+    //       elementToRemove.remove();
+    //     }
+    //   } 
+
+},
 });
 function refresh_location(frm) {
   latitude = +frm.doc.latitude;
   longitude = +frm.doc.longitude;
+  let geojsonFeature = {
+    type: "FeatureCollection",
+    features: [
+      {
+        type: "Feature",
+        properties: {},
+        geometry: { type: "Point", coordinates: [longitude, latitude] },
+      },
+    ],
+  };
+  frm.doc.location = JSON.stringify(geojsonFeature);
   let map = frm.fields_dict.location.map.setView([latitude, longitude], 13);
   L.marker([latitude, longitude]).addTo(map);
 }
